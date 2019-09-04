ff8c5e190283163d575a2d5077023099a34d6b0a
syyad khan <syyadkhan@yahoo.com>
2019-09-04 08:20:47 -0400

    Project Base Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Let's Go!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
