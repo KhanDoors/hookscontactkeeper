4e1a0444e97964f1074ec81d8163e001208df45b
syyad khan <syyadkhan@yahoo.com>
2019-09-09 18:57:35 -0400

    login register form and links created

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/about" component={About} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
