c8362f86138a49cda7c00ea892d811631a598e49
Syyad Khan <syykhan@deloitte.com>
2019-09-10 19:23:04 -0400

    able to register user from front end

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-
+import axios from "axios";
 import authContext from "./authContext";
 import authReducer from "./authReducer";
 import {
@@ -25,15 +25,33 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   //   Load User
-
+  const loadUser = () => console.log("loaduser");
   //   Register User
-
+  const register = async formData => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
   //   Login User
-
+  const login = () => console.log("loginUser");
   //   Logout
-
+  const logout = () => console.log("logoutUser");
   // Clear Errors
-
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
   return (
     <authContext.Provider
       value={{
@@ -41,7 +59,12 @@ const AuthState = props => {
         isAuthenticated: state.isAuthenticated,
         loading: state.loading,
         user: state.user,
-        error: state.error
+        error: state.error,
+        register,
+        loadUser,
+        login,
+        logout,
+        clearErrors
       }}
     >
       {props.children}
